988bbd5379aac6f3dbc68e73db97b37e6f995d37
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2021-04-14 14:30:38 +1000

    arg nms fixes

--- R/fn_make.R
+++ R/fn_make.R
@@ -31,7 +31,6 @@ make_adol_aqol6d_disv_lup <- function ()
 #' @importFrom dplyr select
 #' @importFrom tidyselect all_of
 #' @importFrom purrr discard
-#' @keywords internal
 make_descv_stats_tbl <- function (data_tb, key_var_nm_1L_chr = "round", make_descv_stats_tbl, 
     key_var_vals_chr, variable_nms_chr, test_1L_lgl = F) 
 {
@@ -77,6 +76,91 @@ make_item_plt <- function (tfd_data_tb, var_nm_1L_chr, round_var_nm_1L_chr = "ro
     item_plt <- item_plt + ggplot2::theme(legend.position = legend_position_1L_chr) + 
         ggplot2::scale_fill_manual(values = c("#de2d26", "#fc9272"))
     return(item_plt)
+}
+#' Make qstn resp plots
+#' @description make_qstn_resp_plts() is a Make function that creates a new R object. Specifically, this function implements an algorithm to make qstn resp plots. The function returns Composite (a plot).
+#' @param data_tb Data (a tibble)
+#' @param col_nms_chr Column names (a character vector)
+#' @param lbl_nms_chr Label names (a character vector)
+#' @param plot_rows_cols_pair_int Plot rows columns pair (an integer vector)
+#' @param heights_int Heights (an integer vector)
+#' @param round_var_nm_1L_chr Round variable name (a character vector of length one), Default: 'round'
+#' @return Composite (a plot)
+#' @rdname make_qstn_resp_plts
+#' @export 
+#' @importFrom scales percent_format
+#' @importFrom gridExtra grid.arrange
+#' @importFrom ggpubr ggarrange
+make_qstn_resp_plts <- function (data_tb, col_nms_chr, lbl_nms_chr, plot_rows_cols_pair_int, 
+    heights_int, round_var_nm_1L_chr = "round") 
+{
+    plots_ls <- list()
+    j = 1
+    for (i in col_nms_chr) {
+        tfd_data_tb <- data_tb %>% transform_ds_for_item_plt(var_nm_1L_chr = i, 
+            round_var_nm_1L_chr = round_var_nm_1L_chr)
+        labelx <- lbl_nms_chr[j]
+        j = j + 1
+        plots_ls[[i]] <- make_item_plt(tfd_data_tb, var_nm_1L_chr = i, 
+            round_var_nm_1L_chr = round_var_nm_1L_chr, x_label_1L_chr = labelx, 
+            y_scale_scl_fn = scales::percent_format(), use_bw_theme_1L_lgl = T, 
+            legend_position_1L_chr = "none")
+    }
+    plot_plt <- make_item_plt(tfd_data_tb, var_nm_1L_chr = i, 
+        round_var_nm_1L_chr = round_var_nm_1L_chr, x_label_1L_chr = labelx, 
+        y_scale_scl_fn = NULL, use_bw_theme_1L_lgl = F, legend_position_1L_chr = "bottom")
+    legend_ls <- extract_guide_box_lgd(plot_plt)
+    composite_plt <- gridExtra::grid.arrange(ggpubr::ggarrange(plotlist = plots_ls, 
+        nrow = plot_rows_cols_pair_int[1], ncol = plot_rows_cols_pair_int[2]), 
+        legend_ls, nrow = length(heights_int), heights = heights_int)
+    return(composite_plt)
+}
+#' Make sub ttl plots
+#' @description make_sub_ttl_plts() is a Make function that creates a new R object. Specifically, this function implements an algorithm to make sub ttl plots. The function returns Composite (a plot).
+#' @param data_tb Data (a tibble)
+#' @param col_nms_chr Column names (a character vector)
+#' @param plot_rows_cols_pair_int Plot rows columns pair (an integer vector)
+#' @param round_var_nm_1L_chr Round variable name (a character vector of length one), Default: 'round'
+#' @param make_log_log_tfmn_1L_lgl Make log log transformation (a logical vector of length one), Default: F
+#' @param heights_int Heights (an integer vector)
+#' @return Composite (a plot)
+#' @rdname make_sub_ttl_plts
+#' @export 
+#' @importFrom dplyr mutate
+#' @importFrom stringr str_sub
+#' @importFrom stringi stri_locate_last_fixed
+#' @importFrom gridExtra grid.arrange
+#' @importFrom ggpubr ggarrange
+make_sub_ttl_plts <- function (data_tb, col_nms_chr, plot_rows_cols_pair_int, round_var_nm_1L_chr = "round", 
+    make_log_log_tfmn_1L_lgl = F, heights_int) 
+{
+    plots_ls <- list()
+    for (i in col_nms_chr) {
+        if (make_log_log_tfmn_1L_lgl) {
+            targetvar = paste0("tran_", i)
+            data_tb <- dplyr::mutate(data_tb, `:=`(!!targetvar, 
+                log(-log(1 - !!as.name(i))))) %>% dplyr::mutate(`:=`(!!targetvar, 
+                ifelse(!!as.name(i) == 1, log(-log(1 - 0.999)), 
+                  !!as.name(targetvar))))
+        }
+        labelx <- eval(parse(text = paste0("attributes(data_tb$", 
+            i, ")$label")))
+        labelx <- stringr::str_sub(labelx, start = stringi::stri_locate_last_fixed(labelx, 
+            " - ")[1, 1] %>% unname() + 2)
+        if (make_log_log_tfmn_1L_lgl) {
+            labelx <- paste0("log-log transformed ", labelx)
+        }
+        plots_ls[[i]] <- make_subtotal_plt(data_tb, var_nm_1L_chr = i, 
+            x_label_1L_chr = labelx)
+    }
+    plot_for_lgd_plt <- make_subtotal_plt(data_tb, var_nm_1L_chr = i, 
+        x_label_1L_chr = labelx, legend_position_1L_chr = "bottom", 
+        label_fill_1L_chr = "Data collection")
+    legend_ls <- extract_guide_box_lgd(plot_for_lgd_plt)
+    composite_plt <- gridExtra::grid.arrange(ggpubr::ggarrange(plotlist = plots_ls, 
+        nrow = plot_rows_cols_pair_int[1], ncol = plot_rows_cols_pair_int[2]), 
+        legend_ls, nrow = length(heights_int), heights = heights_int)
+    return(composite_plt)
 }
 #' Make subtotal
 #' @description make_subtotal_plt() is a Make function that creates a new R object. Specifically, this function implements an algorithm to make subtotal plot. The function returns Subtotal (a plot).
--- data-raw/fns/make.R
+++ data-raw/fns/make.R
@@ -54,6 +54,44 @@ make_item_plt <- function(tfd_data_tb,
     ggplot2::scale_fill_manual(values = c("#de2d26","#fc9272"))
   return(item_plt)
 }
+make_qstn_resp_plts <- function(data_tb,
+                                col_nms_chr,
+                                lbl_nms_chr,
+                                plot_rows_cols_pair_int,
+                                heights_int,
+                                round_var_nm_1L_chr = "round"){
+  plots_ls <- list()
+  j=1
+  for(i in col_nms_chr){
+    tfd_data_tb <- data_tb %>%
+      transform_ds_for_item_plt(var_nm_1L_chr = i,
+                                round_var_nm_1L_chr = round_var_nm_1L_chr)
+    labelx <- lbl_nms_chr[j]
+    j = j+1
+    plots_ls[[i]]<- make_item_plt(tfd_data_tb,
+                                  var_nm_1L_chr = i,
+                                  round_var_nm_1L_chr = round_var_nm_1L_chr,
+                                  x_label_1L_chr = labelx,
+                                  y_scale_scl_fn = scales::percent_format(),
+                                  use_bw_theme_1L_lgl = T,
+                                  legend_position_1L_chr = "none")
+  }
+  plot_plt <- make_item_plt(tfd_data_tb,
+                            var_nm_1L_chr = i,
+                            round_var_nm_1L_chr = round_var_nm_1L_chr,
+                            x_label_1L_chr = labelx,
+                            y_scale_scl_fn = NULL,
+                            use_bw_theme_1L_lgl = F,
+                            legend_position_1L_chr = "bottom")
+  legend_ls <- extract_guide_box_lgd(plot_plt)
+  composite_plt <- gridExtra::grid.arrange(ggpubr::ggarrange(plotlist=plots_ls,
+                                                             nrow = plot_rows_cols_pair_int[1],
+                                                             ncol = plot_rows_cols_pair_int[2]),
+                                           legend_ls,
+                                           nrow = length(heights_int),
+                                           heights = heights_int)
+  return(composite_plt)
+}
 make_subtotal_plt <- function(data_tb,
                               var_nm_1L_chr,
                               round_var_nm_1L_chr = "round",
@@ -87,6 +125,44 @@ make_subtotal_plt <- function(data_tb,
     ggplot2::theme(legend.position = legend_position_1L_chr) +
     ggplot2::scale_fill_manual(values = c("#de2d26","#fc9272"))
   return(subtotal_plt)
+}
+make_sub_ttl_plts <- function(data_tb,
+                              col_nms_chr,
+                              plot_rows_cols_pair_int,
+                              round_var_nm_1L_chr = "round",
+                              make_log_log_tfmn_1L_lgl = F,
+                              heights_int ){
+  plots_ls<-list()
+  for(i in col_nms_chr){
+    if(make_log_log_tfmn_1L_lgl){
+      targetvar = paste0("tran_",i)
+      data_tb <- dplyr::mutate(data_tb, !!targetvar := log(-log(1-!!as.name(i))))  %>%
+        dplyr::mutate(!!targetvar :=ifelse(!!as.name(i)==1,log(-log(1-0.999)),!!as.name(targetvar)))
+    }
+    labelx <- eval(parse(text=paste0("attributes(data_tb$",i,")$label")))
+    labelx <- stringr::str_sub(labelx,
+                               start = stringi::stri_locate_last_fixed(labelx," - ")[1,1] %>%
+                                 unname() + 2)
+    if(make_log_log_tfmn_1L_lgl){
+      labelx<- paste0("log-log transformed ", labelx)
+    }
+    plots_ls[[i]]<- make_subtotal_plt(data_tb,
+                                      var_nm_1L_chr = i,
+                                      x_label_1L_chr = labelx)
+  }
+  plot_for_lgd_plt <- make_subtotal_plt(data_tb,
+                                        var_nm_1L_chr = i,
+                                        x_label_1L_chr = labelx,
+                                        legend_position_1L_chr = "bottom",
+                                        label_fill_1L_chr = "Data collection" )
+  legend_ls <- extract_guide_box_lgd(plot_for_lgd_plt)
+  composite_plt <- gridExtra::grid.arrange(ggpubr::ggarrange(plotlist=plots_ls,
+                                                             nrow = plot_rows_cols_pair_int[1],
+                                                             ncol = plot_rows_cols_pair_int[2]),
+                                           legend_ls,
+                                           nrow = length(heights_int),
+                                           heights = heights_int)
+  return(composite_plt)
 }
 make_tableby_cntrls <- function(test_1L_lgl = F){
   tableby_cntrls_ls <- arsenal::tableby.control(
