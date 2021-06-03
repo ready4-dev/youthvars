dc1c4fe51e38c22adc7776158f55fbb28de1b83c
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2021-06-03 12:41:26 +1000

    reattempt following build fail

---
+++ R/db_repln_ds_dict_r3.R
@@ -0,0 +1,13 @@
+#' Data dictionary for study population dataset
+#' 
+#' A data dictionary of the variables used in the source and replication (synthetic) datasets for the First Bounce transfer to utility study
+#' 
+#' A tibble
+#' 
+#' \describe{
+#'   \item{var_nm_chr}{Variable name (a character vector)}
+#'   \item{var_ctg_chr}{Variable category categories (a character vector)}
+#'   \item{var_desc_chr}{Variable description (a character vector)}
+#'   \item{var_type_chr}{Variable type (a character vector)}
+#' }
+"repln_ds_dict_r3"
