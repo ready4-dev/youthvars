ff90224e99afbd6e2c4f3ace38df73ba0c7ae3f1
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2021-12-20 16:07:38 +1100

    updated dpcys and added car to imports

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -16,7 +16,7 @@ x <- ready4fun::make_pkg_desc_ls(pkg_title_1L_chr = "Describe Variables Used To 
                                               "https://ready4-dev.github.io/ready4/")) %>%
   ready4fun::make_manifest(addl_pkgs_ls = ready4fun::make_addl_pkgs_ls(depends_chr = "ready4",
                                                                        suggests_chr = c("rmarkdown"),
-                                                                       imports_chr = c("knitrBootstrap")),#,"car", "caret", 'lme4','quantreg',"recipes"
+                                                                       imports_chr = c("car","knitrBootstrap")),#,"car", "caret", 'lme4','quantreg',"recipes"
                            build_ignore_ls = ready4fun::make_build_ignore_ls(file_nms_chr = c("initial_setup.R")),
                            check_type_1L_chr = "ready4",
                            copyright_holders_chr = "Orygen",
